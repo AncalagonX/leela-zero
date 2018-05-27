6a064808bc3002997fbf59ec924e8459a236e795
AncalagonX <37827291+AncalagonX@users.noreply.github.com>
2018-05-27 21:31:16 +0900

    Properly sorts Lizzie output by LCB Binomial now.

--- src/UCTSearch.cpp
+++ src/UCTSearch.cpp
@@ -60,10 +60,10 @@ public:
     }
 
     friend bool operator<(const OutputAnalysisData& a, const OutputAnalysisData& b) {
-        if (a.m_visits == b.m_visits) {
-            return a.m_winrate < b.m_winrate;
+        if (a.m_winrate == b.m_winrate) {
+            return a.m_visits < b.m_visits;
         }
-        return a.m_visits < b.m_visits;
+        return a.m_winrate < b.m_winrate;
     }
 
 private:
