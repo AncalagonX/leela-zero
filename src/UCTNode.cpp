83ce792de8e3c9584a21895d7772e354004e4968
AncalagonX <37827291+AncalagonX@users.noreply.github.com>
2018-08-03 23:34:12 +0900

    Review mode! This commit forces LZ to provide many more valid search results. It no longer drills down on the single most optimal move available.

--- src/UCTNode.cpp
+++ src/UCTNode.cpp
@@ -337,6 +337,24 @@ UCTNode* UCTNode::uct_select_child(int color, bool is_root, int movenum_now) {
 			continue;
 		}
 
+		int int_m_visits = static_cast<int>(m_visits);
+		int int_child_visits = child.get_visits();
+		if (is_root && int_m_visits >= 5000 && (int_child_visits) > (0.25 * int_m_visits)) {
+			continue;
+		}
+		if (is_root && int_m_visits >= 20000 && (movenum_now2 < 80) && (int_child_visits) > (0.10 * int_m_visits)) {
+			continue;
+		}
+		if (is_root && int_m_visits >= 5000 && int_m_visits < 20000 && (movenum_now2 < 80) && (int_child_visits) > (0.05 * int_m_visits)) {
+			continue;
+		}
+		if (is_root && int_m_visits >= 40000 && (movenum_now2 < 30) && (int_child_visits) > (0.05 * int_m_visits)) {
+			continue;
+		}
+		if (is_root && int_m_visits >= 2000 && int_m_visits < 40000 && (movenum_now2 < 30) && (int_child_visits) > (0.025 * int_m_visits)) {
+			continue;
+		}
+
         auto winrate = fpu_eval;
         if (child.get_visits() > 0) {
             winrate = child.get_eval(color);
